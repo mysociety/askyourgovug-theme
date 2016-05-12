4ea0469f9d00dbefdba6c66ee57d291a6dd0f646
Martin Wright <martin@mynameismartin.com>
2016-05-12 10:34:38 +0100

    Revert AskYourGov to stock Alaveteli
    We will now re-add customisations for AskYourGov back on top of the new Alaveteli base theme. Starting with a copy of commit d3a5a2c54b2222e570c23ef57d4957891730aa92

--- lib/alavetelitheme.rb
+++ lib/alavetelitheme.rb
@@ -1,25 +1,17 @@
-class ActionController::Base
-  before_filter :set_askyourgovug_view_paths
+theme_name = File.split(File.expand_path("../..", __FILE__))[1]
+theme_name.gsub!('-', '_')
+THEME_NAME = theme_name
 
-  def set_askyourgovug_view_paths
+class ActionController::Base
+  # The following prepends the path of the current theme's views to
+  # the "filter_path" that Rails searches when deciding which
+  # template to use for a view.  It does so by creating a method
+  # uniquely named for this theme.
+  path_function_name = "set_view_paths_for_#{THEME_NAME}"
+  before_filter path_function_name.to_sym
+  send :define_method, path_function_name do
     self.prepend_view_path File.join(File.dirname(__FILE__), "views")
   end
-
-  # Note that set_view_paths is called by Alaveteli from the
-  # rescue_action_in_public method, in order that error pages
-  # may be themed correctly. Since askyourgovug-theme is a
-  # primary theme that ought to style error pages, we define
-  # this as an alias
-  alias :set_view_paths :set_askyourgovug_view_paths
-end
-
-# Prepend the asset directories in this theme to the asset path:
-['stylesheets', 'images', 'javascripts', 'docs'].each do |asset_type|
-  theme_asset_path = File.join(File.dirname(__FILE__),
-                               '..',
-                               'assets',
-                               asset_type)
-  Rails.application.config.assets.paths.unshift theme_asset_path
 end
 
 # In order to have the theme lib/ folder ahead of the main app one,
@@ -35,19 +27,28 @@ end
 for patch in ['controller_patches.rb',
               'model_patches.rb',
               'patch_mailer_paths.rb']
-    require File.expand_path "../#{patch}", __FILE__
+  require File.expand_path "../#{patch}", __FILE__
 end
 
 # Note you should rename the file at "config/custom-routes.rb" to
 # something unique (e.g. yourtheme-custom-routes.rb":
 $alaveteli_route_extensions << 'custom-routes.rb'
 
+# Prepend the asset directories in this theme to the asset path:
+['stylesheets', 'images', 'javascripts'].each do |asset_type|
+  theme_asset_path = File.join(File.dirname(__FILE__),
+                               '..',
+                               'assets',
+                               asset_type)
+  Rails.application.config.assets.paths.unshift theme_asset_path
+end
+
 # Tell FastGettext about the theme's translations: look in the theme's
 # locale-theme directory for a translation in the first place, and if
 # it isn't found, look in the Alaveteli locale directory next:
 repos = [
-    FastGettext::TranslationRepository.build('app', :path=>File.join(File.dirname(__FILE__), '..', 'locale-theme'), :type => :po),
-    FastGettext::TranslationRepository.build('app', :path=>'locale', :type => :po)
+  FastGettext::TranslationRepository.build('app', :path=>File.join(File.dirname(__FILE__), '..', 'locale-theme'), :type => :po),
+  FastGettext::TranslationRepository.build('app', :path=>'locale', :type => :po)
 ]
 FastGettext.add_text_domain 'app', :type=>:chain, :chain=>repos
 FastGettext.default_text_domain = 'app'
